69c115fadd2e9fa8d7e646ede9c90c9e2b54f0a9
Толстов Александр <alextav_85@mail.ru>
2019-12-11 12:00:21 +0600

    Add project files.

---
+++ HM1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HM1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawButton_Click(object sender, RoutedEventArgs e)
+        {
+            int numA;
+            int numB;
+            int numC;
+            if(int.TryParse(NumA.Text, out numA) && int.TryParse(NumB.Text, out numB) && int.TryParse(NumC.Text, out numC))
+            {
+                Rectangle Rect = new Rectangle(numA, numB, numC);
+                HorizontNumber.Text = Rect.NumOfRectHorizontal.ToString();
+                VerticalNumber.Text = Rect.NumOfRectVertical.ToString();
+                SummNumber.Text = Rect.NumOfRectSumm.ToString();
+                MessageText.Text = "Все OK";
+            }
+            else
+            {
+                MessageText.Text = "Вы ввели не действительное число попробуйте снова";
+            }
+        }
+    }
+}
---
+++ HM1/Rectangle.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM1
+{
+    public class Rectangle
+    {
+        private int sideA;
+        private int sideB;
+        private int sideC;
+
+        private int numOfRectHorizontal;
+        private int numOfRectVertical;
+        private int numOfRectSumm;
+
+        public int SideA
+        {
+            get
+            {
+                return sideA;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    sideA = value;
+                }
+                else
+                {
+                    sideA = -value;
+                }
+            }
+        }
+        public int SideB
+        {
+            get
+            {
+                return sideB;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    sideB = value;
+                }
+                else
+                {
+                    sideB = -value;
+                }
+            }
+        }
+        public int SideC
+        {
+            get
+            {
+                return sideC;
+            }
+            set
+            {
+                if (value > sideA || value > sideB)
+                {
+                    if (value > sideA)
+                    {
+                        sideC = sideA;
+                    }
+                    if (value > sideB)
+                    {
+                        sideC = sideB;
+                    }
+                }
+                else
+                {
+                    if (value >= 0)
+                    {
+                        sideC = value;
+                    }
+                    else
+                    {
+                        sideC = -value;
+                    }
+                }
+            }
+        }
+        public int NumOfRectSumm
+        {
+            get
+            {
+                return numOfRectSumm;
+            }
+        } //Только возвращает
+        public int NumOfRectHorizontal
+        {
+            get
+            {
+                return numOfRectHorizontal;
+            }
+        } //Только возвращает
+        public int NumOfRectVertical
+        {
+            get
+            {
+                return numOfRectVertical;
+            }
+        } //Только возвращает
+
+        public Rectangle(int SideA = 0, int SideB = 0, int SideC = 0)
+        {
+            this.SideA = SideA;
+            this.SideB = SideB;
+            this.SideC = SideC;
+            Calculate();
+        }
+        public void Calculate()
+        {
+            if (SideA > 0)
+            {
+                numOfRectVertical = SideA / SideC;
+            }
+            else
+            {
+                numOfRectVertical = 0;
+            }
+            if (SideB > 0)
+            {
+                numOfRectHorizontal = SideB / SideC;
+            }
+            else
+            {
+                numOfRectHorizontal = 0;
+            }
+            numOfRectSumm = numOfRectHorizontal * numOfRectVertical;
+        }
+    }
+}
